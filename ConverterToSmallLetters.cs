ef1374907133745fdc3573ba5a36379138e3e4c0
Jberov <67265747+Jberov@users.noreply.github.com>
2022-04-06 23:46:02 +0300

    Adding all class files for every exercise (1-5)

---
+++ ConverterToSmallLetters.cs
@@ -0,0 +1,54 @@
+﻿using MinimalMVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MinimalMVVM.ViewModel
+{
+    public class ConverterToSmallLetters : ObservableObject
+    {
+        public ConverterToSmallLetters()
+        {
+
+        }
+        protected readonly TextConverter _textConverter = new TextConverter(s => s.ToLower());
+        private string _someText;
+        private readonly ObservableCollection<string> _history = (ObservableCollection<string>)Presenter.History;
+
+        public string SomeText
+        {
+            get { return _someText; }
+            set
+            {
+                _someText = value;
+                RaisePropertyChangedEvent("SomeText");
+            }
+        }
+
+        public IEnumerable<string> History
+        {
+            get { return _history; }
+        }
+
+        public ICommand ConvertTextCommand
+        {
+            get { return new DelegateCommand(ConvertText); }
+        }
+
+        private void ConvertText()
+        {
+            AddToHistory(_textConverter.ConvertText(SomeText));
+            SomeText = String.Empty;
+        }
+
+        private void AddToHistory(string item)
+        {
+            if (!_history.Contains(item))
+                _history.Add(item);
+        }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EasyMVVM
+{
+    public class ViewModel : DependencyObject, INotifyPropertyChanged
+    {
+        public ViewModel()
+        {
+            Model m = new Model();
+            BoundProperty = m.GetData();
+        }
+
+        private ObservableCollection<string> _BackingProperty;
+
+        public ObservableCollection<string> BoundProperty
+        {
+            get { return _BackingProperty; }
+            set { _BackingProperty = value; PropChanged("BoundProperty"); }
+        }
+
+        //This event will be fired to notify any listeners of a change in a property value.
+        public event PropertyChangedEventHandler PropertyChanged;
+        //Tell WPF Binding that this property value has changed
+        public void PropChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
